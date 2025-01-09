0688409ba4e6c4d2cb13f6519d2c2caffccab9c2
Sanmesh <sankhesanmesh@gmail.com>
2025-01-09 17:28:42 -0600

    page head change

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'; 
 import Intro from "../components/Intro";
 import Meteors from "@/components/magicui/meteors";
 import WorkExperience from "@/components/WorkExperience";
@@ -11,7 +12,13 @@ import IconCloud from "@/components/ui/icon-cloud";
 
 export default function Home() {
   return (
+    
+    
     <div className="relative lg:w-8/12 md:w-7/12 sm:w-full mx-auto items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] overflow-hidden">
+      <Head>
+        <title>SS_Portfolio</title>
+        <meta name="description" content="Welcome to my portfolio. Explore my work experience, projects, skills, and more!" />
+      </Head>
       
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-full overflow-hidden">
       
